67a395c50b66ce21eb9a20805d00e277174c1398
Eric Brito <eric_brito@msn.com>
2020-05-20 20:44:53 -0300

    Initial Commit

---
+++ MoviesLib/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MoviesLib
+//
+//  Created by Eric Alves Brito on 20/05/20.
+//  Copyright © 2020 DevVenture. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
